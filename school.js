f83f3b8056d1546e20e4bb09110813da1eb5581c
Garima arya <garima.arya1101@gmail.com>
2017-10-06 19:53:46 +0000

    adds looping exercises

---
+++ school.js
@@ -0,0 +1,15 @@
+function whichSchool(age) {
+
+  if (age < 13){
+    console.log("Elementary School");
+  }
+   else if (age > 13 && age < 18){
+     console.log("Secondary School");
+   }
+   else
+   console.log("Lighthouse Labs");
+}
+
+whichSchool(34);
+
+
